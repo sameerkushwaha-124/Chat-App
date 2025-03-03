c4ffb222920c822690156c23dba171d0b2d8e07b
Nikhil Kushwaha <kushwahanikhil2006@gmail.com>
2025-03-03 23:20:30 +0530

    Create a react router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,9 @@
 import React from 'react'
-import "./index.css"
+import AppRoutes from './routes/Approutes'
+
 function App() {
   return (
-    <div>
-      <div className="main bg-yellow-100">
-        <h1 className ="text-sky-950">THis iS KiTT</h1>
-      </div>
-    </div>
+    <AppRoutes/>
   )
 }
 
