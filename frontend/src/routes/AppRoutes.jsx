b1b6b44c0f61c7bdbd1cd9b84b56bcc2dff560c9
Nikhil Kushwaha <kushwahanikhil2006@gmail.com>
2025-03-04 00:15:14 +0530

    created login file

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -6,8 +6,8 @@ const AppRoutes = () => {
     <BrowserRouter>
     <Routes>
         <Route path="/" element={<div>Home</div>}/>
-        <Route path="/login" element={<div>Login</div>}/>
-        <Route path="/Register" element={<div>Register</div>}/>
+        <Route path="/login" element={<Login />}/>
+        <Route path="/register" element={<div>Register</div>}/>
        
     </Routes>
     </BrowserRouter>
