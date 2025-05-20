68e033d04eaf6e9b69eb1408167552b774c9e2d9
Sameer Kushwaha <sameerkushwaha2003@gmail.com>
2025-05-20 21:20:02 +0530

    commit for the backend code

---
+++ backend/routes/message.routes.js
@@ -0,0 +1,10 @@
+import express from 'express';
+import { protectRoute } from '../middlewares/auth.middleware.js';
+import { getMessages, getUsersForSidebar, sendMessage}  from '../controller/message.controller.js';
+const router = express.Router();
+
+router.get('/user', protectRoute, getUsersForSidebar); 
+router.get("/:id", protectRoute, getMessages); 
+router.post('/send/:id', protectRoute, sendMessage);
+
+export default router;
--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,30 @@
-// server.js kyun banaya? 
-// kyunki ham socket IO user karne wale hain jo http module se use karne easy padta hain express ki bajay.
-import dotenv from 'dotenv';
-dotenv.config();
-import http from 'http';
-import app from "./app.js";
+import express from "express";
+import dotenv from "dotenv";
+import {connectDB} from "./config/db.js";
+import cookieParser from 'cookie-parser';
+import cors from "cors";
 
-const server = http.createServer(app);
+import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.routes.js";
+
+
+dotenv.config();
+const app = express();
+
+app.use(cookieParser());
+app.use(express.json()); // Agar tum ise nahi lagate, to req.body undefined rehta hai — jiske wajas se server crash ho sakta hai ya response bhejne se pehle hi connection reset ho jata hai
+app.use(express.urlencoded({extended:true})); // Form data ko parse karne ke liye
+app.use(express.static("public")); // Static files ko serve karne ke liye
+app.use(cors()); // Agar tum ise nahi lagate, to CORS error aata hai jab tum frontend se request bhejte ho   
+
+
+app.use('/api/auth', authRoutes);
+app.use('api/message', messageRoutes);
+
 
 
 const PORT = process.env.PORT || 3000;
-
-server.listen(PORT,(req,res)=>{
-    console.log(`Server is running on port ${PORT}`);
-});
+app.listen(PORT, ()=>{
+    console.log(`Server is running on : ${PORT}`);
+    connectDB();
+})
